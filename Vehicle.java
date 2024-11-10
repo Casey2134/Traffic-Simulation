eeb95837874b4d0b31ed53f159bff7f76a0ec18f
ZeKermet <158208383+ZeKermet@users.noreply.github.com>
2024-11-10 18:45:01 -0500

    Update vehicle class

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,33 @@
 public class Vehicle {
+    int startPoint;
+    int endPoint;
+    double startTime;
+    double endTime;
+    double totalTime;
+    double distanceTraveled;
+
+
+    public Vehicle(int startPosition, int endPosition, double startTime) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+        this.startTime = startTime;
+    };
+
+    public int getStartPoint() {
+        return startPoint;
+    }
+
+    public double getDistanceTraveled() {
+        return distanceTraveled;
+    }
+
+    public double getAverageSpeed() {
+        return (distanceTraveled / totalTime);
+    }
+
+
+    public void doUnitTests() {
+        //.....
+    }
 
 }

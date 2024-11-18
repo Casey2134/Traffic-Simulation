b29e57312635d59b37e47e2ebe4c294e153363fb
lucasportilho13 <lucasportilhopaes@gmail.com>
2024-11-18 09:31:13 -0500

    Highway Lucas 2

--- Highway.java
+++ Highway.java
@@ -2,8 +2,8 @@ import java.util.LinkedList;
 
 public class Highway {
     private LinkedList<Vehicle> lane;
-    private double length; // Total length of the highway in feet
-    private double remainingSpace; // Remaining space on the highway in feet
+    private double length;
+    private double remainingSpace;
 
     // Constructor
     public Highway(double length) {
@@ -12,28 +12,24 @@ public class Highway {
         this.lane = new LinkedList<>();
     }
 
-    // Method to get the total length of the highway
     public double getLength() {
         return this.length;
     }
 
-    // Method to get the remaining space available on the highway
     public double getRemainingSpace() {
         return this.remainingSpace;
     }
 
-    // Method to enqueue a vehicle onto the highway if there's enough space
     public boolean enqueue(Vehicle vehicle) {
         double vehicleLength = vehicle.getVehicleLength();
         if (vehicleLength <= remainingSpace) {
             lane.add(vehicle);
             remainingSpace -= vehicleLength;
-            return true; // Vehicle successfully added to the highway
+            return true;
         }
-        return false; // Not enough space for the vehicle
+        return false; // No space for vehicle
     }
 
-    // Method to dequeue the first vehicle on the highway
     public Vehicle dequeue() {
         if (lane.isEmpty()) {
             return null;
@@ -43,12 +39,10 @@ public class Highway {
         return exitingVehicle;
     }
 
-    // Method to get the number of vehicles currently on the highway
     public int getNumOfVehicles() {
         return lane.size();
     }
 
-    // Method to get the next vehicle at the head of the queue without dequeuing it
     public Vehicle nextVehicle() {
         if (lane.isEmpty()) {
             return null;
@@ -56,28 +50,4 @@ public class Highway {
         return lane.getFirst();
     }
 
-    // Unit test for the Highway class
-    public static void unitTest() {
-        Highway highway = new Highway(500); // Highway of 500 feet
-        Vehicle car1 = new Car(1, 5, 1, 1); // Car with 1 passenger
-        Vehicle car2 = new Car(2, 3, 1, 1); // Car with 2 passengers
-        Vehicle bus1 = new Bus(3, 15, 1, 1); // Bus with 15 passengers
-
-        // Test: Add vehicles to the highway
-        System.out.println("Enqueue Car 1: " + highway.enqueue(car1)); // Expected: true
-        System.out.println("Enqueue Car 2: " + highway.enqueue(car2)); // Expected: true
-        System.out.println("Enqueue Bus 1: " + highway.enqueue(bus1)); // Expected: true if space, false otherwise
-
-        // Test: Get the number of vehicles on the highway
-        System.out.println("Number of vehicles: " + highway.getNumOfVehicles()); // Expected: 2 or 3
-
-        // Test: Check the next vehicle on the highway
-        System.out.println("Next Vehicle: " + highway.nextVehicle()); // Expected: car1
-
-        // Test: Dequeue a vehicle and check remaining space
-        Vehicle exitedVehicle = highway.dequeue();
-        System.out.println("Dequeued Vehicle: " + exitedVehicle);
-        System.out.println("Remaining Space: " + highway.getRemainingSpace()); // Should reflect space after removing
-                                                                               // car1
-    }
 }

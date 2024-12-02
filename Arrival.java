6dc2cada819cb2e47d5bae9a2d2fbfabcb5445fa
TristanEakin <144827387+TristanEakin@users.noreply.github.com>
2024-12-01 22:01:33 -0500

    Why Intellij, WHYYYY???!!!

--- Arrival.java
+++ Arrival.java
@@ -6,29 +6,37 @@ public class Arrival {
     private double timeLeftToBuses;
     private double intervalForBuses;
     private int totalBusPassengers = 0;
+    private int numberOfBuses;
 
-    public Arrival(double intervalForBuses) {
+    public Arrival(double intervalForBuses, int numberOfBuses) {
         bus = new Normal((Bus.getMaxPassengers() / 2), 3);
         car = new Normal((Car.getMaxPassengers() / 2), 1);
+        this.numberOfBuses = numberOfBuses;
         this.intervalForBuses = intervalForBuses;
-        timeLeftToBuses = intervalForBuses;
+        timeLeftToBuses = 0;
     }
 
     // Creation Method For Vehicle
-    public Vehicle nextVehicle(double currentTime, Highway highway, Highway exitHighway, int totalHighways) {
+    public Vehicle nextVehicle(double currentTime, Highway highway, Highway exitHighway) {
         Vehicle vehicle;
         int passengers;
-        if(currentTime > timeLeftToBuses){
-            busesLeft = totalHighways;
+        if(currentTime >= timeLeftToBuses){
+            busesLeft = numberOfBuses;
             timeLeftToBuses += intervalForBuses;
         }
         if(busesLeft > 0){
             passengers = (int) bus.sample();
+            if(passengers <= 0){
+                passengers = 1;
+            }
             totalBusPassengers += passengers;
             vehicle = new Bus(passengers, highway, exitHighway, currentTime);
             busesLeft--;
         } else{
             passengers = (int) car.sample();
+            if(passengers <= 0){
+                passengers = 1;
+            }
             if(totalBusPassengers > 0){
                 totalBusPassengers -= passengers;
                 return(null);
